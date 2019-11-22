f1c2d269c623c936b91dd64cf8680daed93199b0
SpaceCadet0 <r341522000@gmail.com>
2019-11-22 15:33:29 +0500

    gz 2211

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
     //menu scroll
-        
-    const anchors = document.querySelectorAll('a[href*="#"]')
+
+    const anchors = document.querySelectorAll('a[href*="#m-"]')
 
     for (let anchor of anchors) {
         anchor.addEventListener('click', function (e) {
@@ -14,24 +14,26 @@
             })
         })
     }
- 
-        
+
+
     // scrollup
-    function ScrollUp(){
-        var t,s;
-        s=document.body.scrollTop||window.pageYOffset;
-        t=setInterval(function(){if(s>0)window.scroll(1,s-=50);else clearInterval(t)},1);
+    function ScrollUp() {
+        var t, s;
+        s = document.body.scrollTop || window.pageYOffset;
+        t = setInterval(function () {
+            if (s > 0) window.scroll(1, s -= 50);
+            else clearInterval(t)
+        }, 1);
     }
 
 
 
     //appear
     var div = document.getElementById('up__btn');
-    window.onscroll = function() {
+    window.onscroll = function () {
         if (window.pageYOffset > 100) {
             div.classList.add('visible');
-        }
-        else if (window.pageYOffset < 100) {
+        } else if (window.pageYOffset < 100) {
             div.classList.remove('visible');
         }
     };
@@ -61,8 +63,21 @@
         }
     })
 
+    //modal 
+    var btnRadius = $('.cd-modal-bg').width() / 2,
+        left = $('.cd-modal-bg').offset().left + btnRadius,
+        top = $('.cd-modal-bg').offset().top + btnRadius - $(window).scrollTop(),
+        scale = scaleValue(top, left, btnRadius, $(window).height(), $(window).width());
 
-            //carousel
+    function scaleValue(topValue, leftValue, radiusValue, windowW, windowH) {
+        var maxDistHor = (leftValue > windowW / 2) ? leftValue : (windowW - leftValue),
+            maxDistVert = (topValue > windowH / 2) ? topValue : (windowH - topValue);
+        return Math.ceil(Math.sqrt(Math.pow(maxDistHor, 2) + Math.pow(maxDistVert, 2)) / radiusValue);
+    }
+
+
+
+    //carousel
     //  /* этот код помечает картинки, для удобства разработки */
     //  let i = 1;
     //  for(let li of carousel.querySelectorAll('li')) {
@@ -70,16 +85,16 @@
     //    li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
     //    i++;
     //  }
- 
+
     //  /* конфигурация */
     //  let width = 130; // ширина картинки
     //  let count = 3; // видимое количество изображений
- 
+
     //  let list = carousel.querySelector('ul');
     //  let listElems = carousel.querySelectorAll('li');
- 
+
     //  let position = 0; // положение ленты прокрутки
- 
+
     //  carousel.querySelector('.prev').onclick = function() {
     //    // сдвиг влево
     //    position += width * count;
@@ -87,7 +102,7 @@
     //    position = Math.min(position, 0)
     //    list.style.marginLeft = position + 'px';
     //  };
- 
+
     //  carousel.querySelector('.next').onclick = function() {
     //    // сдвиг вправо
     //    position -= width * count;
